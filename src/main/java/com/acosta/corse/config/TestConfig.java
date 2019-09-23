8a1e26ef99fb0d69300bf96bae751beb656f55fc
Alberto Costa <mihcadevs@gmail.com>
2019-09-22 22:11:22 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/acosta/corse/config/TestConfig.java
+++ src/main/java/com/acosta/corse/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.acosta.corse.entities.Category;
 import com.acosta.corse.entities.Order;
+import com.acosta.corse.entities.OrderItem;
 import com.acosta.corse.entities.Product;
 import com.acosta.corse.entities.User;
 import com.acosta.corse.entities.enums.OrderStatus;
 import com.acosta.corse.repositories.CategoryRepository;
+import com.acosta.corse.repositories.OrderItemRepository;
 import com.acosta.corse.repositories.OrderRepository;
 import com.acosta.corse.repositories.ProductRepository;
 import com.acosta.corse.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	
 
@@ -73,6 +78,13 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
 	}
 	
